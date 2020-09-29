7ca868a224e9be35beb776bec4f4a5acdde79230
Glauco Mendes <Glauco_Mendes@hotmail.com>
2020-09-29 17:36:22 -0300

    Trabalho2

---
+++ Trabalho2/frontend/src/App.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import api from './api';
+import StudentForm from './Components/StudentForm';
+import './App.css';
+export default class App extends React.Component {
+    state = {
+        students: [],
+        editForm: undefined
+    }
+
+    componentDidMount() {
+        this.getStudents();
+    }
+    getStudents = async () => {
+        let data = await api.get('/students').then(({ data }) => data);
+        this.setState({ students: data });
+    }
+    createStudent = async (student) => {
+        await api.post('/students', student);
+        this.getStudents();
+    }
+    deleteStudent = async (id) => {
+        await api.delete(`/students/${id}`)
+        this.getStudents();
+    }
+    updateStudent = async (id, val) => {
+        await api.patch(`/students/${id}`, val)
+        this.getStudents();
+    }
+
+    render() {
+        return (
+            <div>
+
+                <h2>Registrar Estudante</h2>
+                <StudentForm func={this.createStudent} />
+                <br /><br />
+                <table border='1'>
+                    <thead >
+                        <tr>
+                            <td>Estudante</td>
+                            <td>Nota 1</td>
+                            <td>Nota 2</td>
+                            <td>Média</td>
+                            <td>Deletar</td>
+                            <td>Editar</td>
+                            
+                        </tr>
+
+                        {this.state.students.map(student => {
+                            return <tr key={student.id}>
+                                <td>{student.name}</td>
+                                <td>{student.grade1}</td>
+                                <td>{student.grade2}</td>
+                                <td>{(parseFloat(student.grade1) + parseFloat(student.grade2))/2}</td>
+                                <td><button onClick={() => this.deleteStudent(student.id)}>X</button></td>
+                                <td><button onMouseUp={() => {
+                                    this.setState({ editForm: <div></div> })
+                                }} onClick={() => {
+                                    this.setState({
+                                        editForm:
+                                            <div id='divUpdate'>
+                                                <h3>Editar Estudante {student.name}</h3>
+                                                <StudentForm student={student} func={this.updateStudent} func0={() => this.setState({ editForm: <div></div> })} />
+                                            </div>
+                                    })
+                                }}>Edit</button></td>
+                               
+                            </tr>
+                        })}
+                    </thead>
+                </table>
+
+                {this.state.editForm}
+
+
+            </div>);
+    }
+
+}
---
+++ Trabalho2/frontend/src/Components/StudentForm.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export default class StudentForm extends React.Component {
+
+    constructor(props) {
+        super();
+     
+        this.isUpdate = false;
+        if (props.student != null)
+            this.isUpdate = true;
+        this.state = {
+            name: props?.student?.name ?? undefined,
+            grade1: props?.student?.grade1 ?? undefined,
+            grade2: props?.student?.grade2 ?? undefined
+        }
+    }
+    handleNameChange = (event) => {
+        this.setState({ name: event.target.value });
+    }
+    handleGrade1Change = (event) => {
+        this.setState({ grade1: event.target.value });
+    }
+    handleGrade2Change = (event) => {
+        this.setState({ grade2: event.target.value });
+    }
+     refresh = 0;
+    render() {
+        return <React.Fragment>
+            <label>Estudante:</label>
+            <input type='text' value={this.state.name} onChange={this.handleNameChange} />
+            <label>Nota 1:</label>
+            <input type='text' value={this.state.grade1} onChange={this.handleGrade1Change} />
+            <label>Nota 2:</label>
+            <input type='text' value={this.state.grade2} onChange={this.handleGrade2Change} />
+            <input type='submit' value={this.isUpdate ? 'Atualizar' : 'Criar'}
+                onClick={() => {
+                    if (this.isUpdate)
+                        this.props.func0();
+                    this.isUpdate
+                        ? this.props.func(this.props.student.id, this.state)
+                        : this.props.func(this.state)
+                }} />
+        </React.Fragment>
+    }
+}
